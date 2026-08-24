b93e03d25a92048b96f5264664461e9f8b51b193
Musiquammation <bryanmegherbi@gmail.com>
2026-08-24 12:02:37 +0200

    fix Logger

--- server/Logger.ts
+++ server/Logger.ts
@@ -1,5 +1,6 @@
-import fs from "node:fs";
-import path from "node:path";
+import "dotenv/config";
+import fs from "fs";
+import path from "path";
 
 export type LoggerLevel = "debug" | "info" | "waring" | "error";
 
