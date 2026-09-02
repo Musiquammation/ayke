670dfbef6c16fa2bf3be485e0accb097f6f33d17
Musiquammation <bryanmegherbi@gmail.com>
2026-09-02 14:37:27 +0200

    Setup capacitor

--- vite.config.ts
+++ vite.config.ts
@@ -1,28 +1,42 @@
 import { defineConfig } from "vite";
 
-export default defineConfig(({ command }) => {
+export default defineConfig(({ command, mode }) => {
 	if (command === "serve") {
 		return {
 			root: "client"
 		};
 	}
 
+	if (mode === "bundle") {
+		return {
+			root: "client",
+
+			build: {
+				outDir: "dist",
+				emptyOutDir: true,
+				minify: false,
+
+				rollupOptions: {
+					input: "src/index.ts",
+					preserveEntrySignatures: "strict",
+
+					output: {
+						entryFileNames: "bundle.js",
+						format: "es",
+						assetFileNames: "[name][extname]"
+					}
+				}
+			}
+		};
+	}
+
 	return {
+		root: "client",
+
 		build: {
 			outDir: "dist",
 			emptyOutDir: true,
-			minify: false,
-
-			rollupOptions: {
-				input: "client/src/index.ts",
-				preserveEntrySignatures: "strict",
-
-				output: {
-					entryFileNames: "bundle.js",
-					format: "es",
-					assetFileNames: "[name][extname]"
-				}
-			}
+			minify: false
 		}
 	};
 });
