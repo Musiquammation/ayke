dcbcfd719fb06362b36a01a19aed419445255efc
Musiquammation <bryanmegherbi@gmail.com>
2026-09-05 11:28:38 +0200

    Improve ghpages.sh

--- scripts/ghpages.sh
+++ scripts/ghpages.sh
@@ -1,12 +1,8 @@
-git switch master
-npm run build
-find client/dist -type f -name '*.js' -delete
-npm run build-bundle
-cp client/index.html dist/
-
-git switch gh-pages
-cp -r client/dist/. public/
+git switch master &&
+npm run build &&
+find client/dist -type f -name '*.js' -delete &&
+npm run build-bundle &&
+cp client/index.html dist/ &&
+git switch gh-pages &&
+cp -r client/dist/. public/ &&
 python3 ./scripts/copyIndexHtml.py
-
-
-
