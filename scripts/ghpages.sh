71d64301109bb43095ce6ded90b9bad108c89c21
Musiquammation <bryanmegherbi@gmail.com>
2026-09-06 20:47:06 +0200

    ghpages.sh

--- scripts/ghpages.sh
+++ scripts/ghpages.sh
@@ -1,10 +1,11 @@
 git switch master &&
 rm -rf client/dist &&
-cp -r client/public client/dist &&
 npm run bundle &&
 rm -r client/dist/assets &&
-cp client/index.html client/dist &&
+cp -r client/public client/dist &&
+cp client/index.html client/dist/index.html &&
 cp scripts/copyIndexHtml.py client/dist/copyIndexHtml.py &&
 git switch gh-pages &&
-cp -r client/dist/. public/ &&
+rm -rf public/
+cp -r client/dist/public/. public/ &&
 python3 client/dist/copyIndexHtml.py
