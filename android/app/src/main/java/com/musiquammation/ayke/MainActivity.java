6c202f0354309d145580dbdbbc03ac7f9454f673
Musiquammation <bryanmegherbi@gmail.com>
2026-09-06 22:02:59 +0200

    android config

---
+++ android/app/src/main/java/com/musiquammation/ayke/MainActivity.java
@@ -0,0 +1,5 @@
+package com.musiquammation.ayke;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
--- client/mobile/mobile.ts
+++ client/mobile/mobile.ts
@@ -1,5 +1,6 @@
 import { StatusBar } from '@capacitor/status-bar';
 
 export async function initMobile() {
+	alert("Hello from mobile.ts");
 	await StatusBar.hide();
 }
